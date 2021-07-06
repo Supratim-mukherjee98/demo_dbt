7100224586cb7b9f6a77b03df9064a1f31fe1131
supratim@atlan.com <supratim@atlan.com>
2021-07-06 18:18:05 +0000

    2nd test

---
+++ target/run/my_new_project/models/example/my_second_dbt_model.sql
@@ -0,0 +1,8 @@
+
+  create or replace  view UTIL_DB.Atlan_dbt.my_second_dbt_model  as (
+    -- Use the `ref` function to select from other models
+
+select *
+from UTIL_DB.Atlan_dbt.my_first_dbt_model
+where id = 1
+  );
